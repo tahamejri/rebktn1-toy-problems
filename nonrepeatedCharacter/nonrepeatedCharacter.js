0b362ca261fb1fa02c372af5407e3c0aedaf754a
tahamejri <taha.mejri13@gmail.com>
2019-11-25 09:01:23 +0100

    toy problem 1

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -5,7 +5,22 @@
  *   firstNonRepeatedCharacter('ABA'); // => 'B'
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
-
+//the idea is to look for the unrepeated characters and push em in an array,
+// the first one will be at the first index so we return it
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var arrayOfNonRepeated = [] ;
+  var repeatedChars = [] ;
+  for(var i=0 ; i<string.length ; i++){
+  	for(var j = i+1 ; j<string.length ; j++){
+  		if(string[i] === string[j]){
+  			repeatedChars.push(string[i])
+  		}
+
+  	}
+  	if(repeatedChars.indexOf(string[i]) === -1){
+  		arrayOfNonRepeated.push(string[i])
+  	}
+
+  }
+  return arrayOfNonRepeated[0]
 };
